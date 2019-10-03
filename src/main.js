03d1be6a22629b435f6524bba8224a20459f86f6
cjkumaresh <test@test.com>
2019-10-03 10:38:34 +0530

    css setup

--- src/main.js
+++ src/main.js
@@ -1,13 +1,27 @@
 /**
  * main.js
- * 
+ *
  * entry file for the app
  */
 import React from "react";
 import ReactDOM from "react-dom";
+import FontFaceObserver from "fontfaceobserver";
+import "sanitize.css/sanitize.css";
 
 // root app
 import App from "./containers/App";
+
+const openSansObserver = new FontFaceObserver("Mansalva", {});
+
+// When Open Sans is loaded, add a font-family using Open Sans to the body
+openSansObserver.load().then(
+  () => {
+    document.body.classList.add("fontLoaded");
+  },
+  () => {
+    console.log("Font not available");
+  }
+);
 
 const MOUNT_NODE = document.getElementById("app");
 const APP_NAME = "Json Resume";
